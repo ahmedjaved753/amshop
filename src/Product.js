3ea0994d9f131a24f861026bffe92f91d0595af5
andinetmn@gmail.com <andinetmn@gmail.com>
2021-01-15 23:18:41 -0600

    first commit

--- src/Product.js
+++ src/Product.js
@@ -25,7 +25,7 @@ function Product({id,title,image,price,rating}) {
             <div className="product_info"></div>
             <p>{title}</p>
             <p className="product_price">
-                <small>$</small>
+                <small>CFA</small>
                 <strong>{price}</strong>
             </p>
            <div className="product_rating">
